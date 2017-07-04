202923d6bc7901a9859bb7d5d2c32f7d43638162
James Sorensen <james@ecosleuth.com>
2017-07-04 13:01:44 -0600

    Run composer update on lightning distribution

--- docroot/index.php
+++ docroot/index.php
@@ -16,7 +16,7 @@ $autoloader = require_once 'autoload.php';
 $kernel = new DrupalKernel('prod', $autoloader);
 
 $request = Request::createFromGlobals();
-$response = $kernel->handle($request);https://www.netflix.com/watch/70292060?trackId=14170286&tctx=1%2C0%2Cf7133bc8-baa7-4604-8272-553de59bec85-185138427
+$response = $kernel->handle($request);
 $response->send();
 
 $kernel->terminate($request, $response);
